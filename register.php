66992252b2cc9a85c1724d20f5af9f204b8d8fcc
DominiqueGregoire <dominique.gregoire@my.jcu.edu.au>
2022-05-01 19:17:26 +1000

    create a blank sign-up form.

--- register.php
+++ register.php
@@ -1,1 +1,17 @@
-<?php
+<?php include("dbconnect.php"); ?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>The Advice Shop - Sample Advice</title>
+    <link href="styles/mainstyles.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+
+<body>
+<?php include("inc_header.php");
+include("inc_nav.php"); ?>
+
+<h1>Sign Up Form</h1>
+<?php include("inc_footer.php"); ?>
+</body>
