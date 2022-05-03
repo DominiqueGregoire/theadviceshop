79090ef5b8d130314fbb96d6980f0acbfa5dfe6b
DominiqueGregoire <dominique.gregoire@my.jcu.edu.au>
2022-05-03 17:11:45 +1000

    change username to uname for the session in login.php

--- login.php
+++ login.php
@@ -1,7 +1,9 @@
 <?php
 error_reporting(E_ALL);
 session_start();
-$_SESSION['username'] = $_REQUEST['username'];
-//echo $_GET['page'];
+$_SESSION['uname'] = $_REQUEST['username'];
+$_SESSION['pword'] = $_REQUEST['password'];
+
+echo $_GET['page'];
 header("Location: ".$_GET['page'].".php");
 exit();
