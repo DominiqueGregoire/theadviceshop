6c41221f59d7f7a76501c7d7dad2d24fb930b4a0
DominiqueGregoire <dominique.gregoire@my.jcu.edu.au>
2022-05-11 23:43:11 +1000

    fix issue of not being able to view page when submit button is pressed.

--- register.php
+++ register.php
@@ -1,5 +1,4 @@
-<?php include("dbconnect.php");
-?>
+<?php include("dbconnect.php");?>
 
 <!doctype html>
 <html lang="en">
@@ -10,30 +9,22 @@
 </head>
 
 <body>
-<?php include("inc_header.php");include("inc_nav.php"); ?>
+<?php include("inc_header.php");
+include("inc_nav.php"); ?>
 
 <?php
 // define variables and set to empty values
 $usernameErr = $emailErr = $passwordErr = "";
 $username = $email = $password = "";
 
-//if the form is submitted validate it else skip and display a blank form
-// check the username
-/**
- * @param string $usernameErr
- * @param string $username
- * @param string $emailErr
- * @param string $email
- * @return array
- */
-function extracted(string $usernameErr, string $username, string $emailErr, string $email): array
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // check the username
     if (empty($_POST["username"])) {
         $usernameErr = "Username is required";
     } else {
         $username = test_input($_POST["username"]);
 // check if username only contains letters and numbers
-        if (!preg_match("/^[a-zA-Z-\d) ]*$/", $username)) {
+        if (!preg_match("/^[a-zA-Z-\d)]*$/", $username)) {
             $usernameErr = "No spaces,only letters and numbers allowed";
         }
     }
@@ -48,65 +39,50 @@ function extracted(string $usernameErr, string $username, string $emailErr, stri
             $emailErr = "Invalid email format";
         }
     }
-    return array($usernameErr, $username, $emailErr, $email);
-}
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    list($usernameErr, $username, $emailErr, $email) = extracted($usernameErr, $username, $emailErr, $email);
-
-//check the password
-if (empty($_POST["password"])) {
-$passwordErr = "Password is required";
-} else {
-$password = test_input($_POST["password"]);
+    //check the password
+    if (empty($_POST["password"])) {
+        $passwordErr = "Password is required";
+    } else {
+        $password = test_input($_POST["password"]);
 // check if password has 8 characters and contains at least 1 uppercase, 1 lowercase, 1 number
-    if (!preg_match("/^[a-zA-Z\d]*$/", $password) || strlen($password) < 9)  {
-        $passwordErr = "Password must have 8 characters and contain one number, one uppercase and 
+        if (!preg_match("/^[a-zA-Z\d]*$/", $password) || strlen($password) < 9) {
+            $passwordErr = "Password must have 8 characters and contain one number, one uppercase and 
         one lowercase letter.";
+        } else {
+            echo "You have successfully signed up, Click on login to login";
+        }
     }
 } //end else
 
 // create the test input function
-function test_input($data)  {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 } //end test-input
-}
 ?>
 
-<h1>Sign Up Form</h1>
-
-<div class="register">
-    <p><span class="error">required field</span></p>
-
-    <form  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
-           method="post">
-
-        Username : <label>
-            <input type="text" name="username" value="<?php echo $username;?>">
-            </label>
-        <span class="error">* <?php echo $usernameErr;?></span>
-        <br><br>
-
-        Email : <label>
-            <input type="text" name="email" value="<?php echo $email;?>">
-            </label>
-        <span class="error">* <?php echo $emailErr;?></span>
-        <br><br>
-
-        Password : <label>
-            <input type="text" name="password" value="<?php echo $password;?>">
-            </label>
-        <span class="error">* <?php echo $passwordErr;?></span>
-        <br><br>
-        <span><input type="submit" name="submit" value="Submit"></span>
-        <br><br>
+<h2 id="h2signup">Sign Up Form</h2>
+<div id="box">
+    <p><span class="error">* required fields</span></p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div id="container">
+            <form action="register.php" method="post">
+                Username : <input type="text" name="username" value="<?php echo $username; ?>">
+                    <span class="error">*<?php echo $usernameErr; ?></span> <br><br>
+                Email: <input type="text" name="email" value="<?php echo $email; ?>">
+                    <span class="error">*<?php echo $emailErr; ?></span> <br><br>
+                Password : <input type="text" name="password" value="<?php echo $password; ?>">
+                    <span class="error">*<?php echo $passwordErr; ?></span> <br><br>
+                <input class="signupbutton" type="submit" name="submit" value="Sign up"><br><br>
+                <a href="login.php">Click to log in</a><br><br>
+            </form>
+        </div>
     </form>
-
 </div>
 
-
-<?php include("inc_footer.php"); ?>
+<?php //include("inc_footer.php"); ?>
 </body>
