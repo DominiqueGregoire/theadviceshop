07efacba95e65283c7f27293c5a025fe764a0d75
DominiqueGregoire <dominique.gregoire@my.jcu.edu.au>
2022-05-12 15:56:43 +1000

    create the login form. note there is still an issue with checking for wrong inputs.

--- login.php
+++ login.php
@@ -1,16 +1,5 @@
 <?php include("dbconnect.php")?>
 
-<?php
-error_reporting(E_ALL);
-session_start();
-$_SESSION['username'] = $_REQUEST['username'];
-$_SESSION['password'] = $_REQUEST['password'];
-
-//echo $_GET['page'];
-//header("Location: " . $_GET['page'] . ".php");
-//exit();
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -21,21 +10,50 @@ $_SESSION['password'] = $_REQUEST['password'];
 
 <body>
 
-<?php include("inc_header.php");include("inc_nav.php");?>
+<?php include("inc_header.php");
+include("inc_nav.php");?>
 
+<?php
+//error_reporting(E_ALL);
+session_start();
+$_SESSION['username'] = $_REQUEST['username'];
+$_SESSION['password'] = $_REQUEST['password'];
+
+// define variables and set to empty values
+$username = $password = "";
+$usernameErr = $passwordErr = "";
+
+// check that the username and password are correct
+// there is an issue here that if wrong username or password is entered, no error message is displayed
+$msg = '';
+if (isset($_POST['login']) && !empty($_POST['username'])
+    && !empty($_POST['password']))  {
+    if (($_POST['username'] == $_SESSION['username']) &&
+    $_POST['password'] == $_SESSION['password']) {
+        $_SESSION['valid'] = true;
+        $_SESSION['timeout'] = time();
+        $_SESSION['username'] = $username;
+        echo 'You have entered valid username and password';
+    } else {
+        echo 'Username and password are incorrect';
+    }
+}
+?>
+
+<h2 id="h2login">Login</h2>
 <div id="box">
-    <form action="login.php"
-          method="post">
-        Username : <input type="text" name="username"><br><br>
-        Password : <input type="text" name="password"><br><br>
-
-       <input type="button" value="login"><br><br>
+<div id="container">
+    <form action="login.php<?php htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        Username : <input type="text" name="username" value="<?php echo $username; ?>">
+        <span class="error">*<?php echo $usernameErr; ?></span> <br><br>
+        Password : <input type="text" name="password" value="<?php echo $password; ?>">
+        <span class="error">*<?php echo $passwordErr; ?></span> <br><br>
+        <input class="loginbutton" type="submit" name="submit" value="Login"><br><br>
 
         <a href="register.php">Signup</a><br><br>
     </form>
 </div>
-
-<?php //include("inc_footer.php"); ?>
+</div>
+<?php include("inc_footer.php"); ?>
 </body>
-<!--        action="--><?php //echo htmlspecialchars($_SERVER["PHP_SELF"]);?><!--"-->
 
